1dd32dd31b2fc08335e065f2403679641c9eee8c
phil-wew <pwewiora08@Gmail.com>
2020-09-21 12:44:43 -0700

    table problems

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -15,14 +15,3 @@ function tableDisplay(ufoSightings) {
         });
     });
 };
-
-
-var button = d3.select("#filter-btn");
-
-button.on("click", function() {
-
-            // Prevent the page from refreshing
-            d3.event.preventDefault();
-
-            // Select the input element and get the raw HTML node
-            var inputElement = d3.select("#patient-form-input");
