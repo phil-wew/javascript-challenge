6b7853a940d607b76e14993f69033222001ff856
phil-wew <pwewiora08@Gmail.com>
2020-09-21 13:10:42 -0700

    filtering

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -4,11 +4,13 @@ var tableData = data;
 // function to display UFO sightings
 function tableDisplay(ufoSightings) {
 
+
     var tbody = d3.select("tbody");
+    //loop through sightings - append row with data
     ufoSightings.forEach((ufo) => {
         var row = tbody.append("tr");
 
-
+        // loop through key/values append data to cells
         Object.entries(ufo).forEach(([key, value]) => {
             var cell = row.append("td");
             cell.text(value);
@@ -16,3 +18,27 @@ function tableDisplay(ufoSightings) {
     });
 };
 tableDisplay(tableData);
+
+//////// Filtering Table ////////
+
+
+
+// select filter button
+var button = d3.select("#filter-btn");
+
+//define button click
+button.on("click", function() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    var filteredData = tableData.filter(ufo => ufo.datetime === inputValue)
+
+    tableDisplay(filteredData);
+});
