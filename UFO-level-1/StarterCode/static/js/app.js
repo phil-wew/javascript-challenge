49dd35ee50736da722e44f789f51a047a487d8ce
phil-wew <pwewiora08@Gmail.com>
2020-09-21 11:42:45 -0700

    table js

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -1,3 +1,17 @@
 // from data.js
 var tableData = data;
-var tbody = d3.select("tbody");
+
+// function to display UFO sightings
+function tableDisplay(ufoSightings) {
+
+    var tbody = d3.select("tbody");
+    ufoSightings.forEach((ufo) => {
+        var row = tbody.append("tr");
+
+
+        Object.entries(ufo).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
